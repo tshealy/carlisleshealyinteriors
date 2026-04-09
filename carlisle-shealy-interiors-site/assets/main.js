83850fa8b39d45627bbaec156aaf4baf954a5202
Tripp Shealy <tripp.shealy@gmail.com>
2026-04-09 11:33:01 -0400

    Delete carlisle-shealy-interiors-site directory

--- carlisle-shealy-interiors-site/assets/main.js
+++
@@ -1,7 +0,0 @@
-
-const links = document.querySelectorAll('nav a');
-const current = window.location.pathname.split('/').pop() || 'index.html';
-links.forEach(link => {
-  const href = link.getAttribute('href');
-  if (href === current) link.classList.add('active');
-});
